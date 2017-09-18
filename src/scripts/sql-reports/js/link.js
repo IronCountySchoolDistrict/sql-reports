d7f61d924efe885b39201989d431b74d22e8c3e9
jonesnc <purkinje90@gmail.com>
2017-09-18 10:49:47 -0600

    Reporttabs ul should be checked if it already has ICSDReports link before inserting the link

--- src/scripts/sql-reports/js/link.js
+++ src/scripts/sql-reports/js/link.js
@@ -1,6 +1,10 @@
 import $ from 'jquery';
 
 export function main() {
-  var tabTemplate = $('#sql-reports-tab').html();
-  $('ul.tabs').prepend(tabTemplate);
+  // BUG: this script may be executed twice, so check if the ICSDReports link has already been
+  // inserted
+  if (!$('ul.tabs li:contains("ICSDReports")').length > 0) {
+    var tabTemplate = $('#sql-reports-tab').html();
+    $('ul.tabs').prepend(tabTemplate);
+  }
 }
