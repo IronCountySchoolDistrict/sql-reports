812b2f3e53e1d0637ebcff90418d2b3aef9622ba
Nathan Jones <nathan.jones@ironmail.org>
2017-11-09 17:05:29 -0700

    Make sure the ICSDReports link hasn't been inserted yet

--- src/scripts/sql-reports/js/link.js
+++ src/scripts/sql-reports/js/link.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 
 export function main() {
-  var tabTemplate = $('#sql-reports-tab').html();
-  $('ul.tabs').prepend(tabTemplate);
+  if (!$('ul.tabs li:contains("ICSDReports")').length > 0) {
+    var tabTemplate = $('#sql-reports-tab').html();
+    $('ul.tabs').prepend(tabTemplate);
+  }
 }
