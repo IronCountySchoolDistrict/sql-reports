c36cebf53437bea6c63ab4c06d6386c338d9fb53
Nathan Jones <nathan.jones@ironmail.org>
2017-12-05 16:16:40 -0700

    Revert "Use default function for link insertion js module"
    This reverts commit 6f81a4a638c59bb989ecd1403d93737962a7b45f.

--- src/scripts/sql-reports/js/link.js
+++ src/scripts/sql-reports/js/link.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-export default function() {
+export function main() {
   if (!$('ul.tabs li:contains("ICSDReports")').length > 0) {
     var tabTemplate = $('#sql-reports-tab').html();
     $('ul.tabs').prepend(tabTemplate);
